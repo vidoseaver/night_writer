ce2a99d72e531305d9d36448b94e6e10df7783b3
Vido <vidoseaver@gmail.com>
2016-07-13 19:06:43 -0600

    starting pairing w/ hiedi

--- lib/braille_parser.rb
+++ lib/braille_parser.rb
@@ -8,19 +8,18 @@ class BrailleParser
     @top_line    = []
     @middle_line = []
     @bottom_line = []
+  end
 
-  def chunk(string, size)
+  def chunker(string, size)
     string.scan(/.{1,#{size}}/)
   end
 
-  def split_string(braille)
-
-  	braille.each do |num|
+  def split_string
+  	@input.each do |num|
   	    @top_line    << num[0..1]
   	    @middle_line << num[2..3]
   	    @bottom_line << num[4..5]
   	end
-
   end
 
   def joiner
@@ -29,16 +28,24 @@ class BrailleParser
     @bottom_line = @bottom_line.join
   end
 
+  def chunker_applied
+    @top_line = chunker(@top_line, 160)
+    @middle_line = chunker(@middle_line, 160)
+    @bottom_line = chunker(@bottom_line, 160)
+  end
 
-@top_line = chunk(@top_line, 160)
-@middle_line = chunk(@middle_line, 160)
-@bottom_line = chunk(@bottom_line, 160)
-if @top_line[0].length < 160
-	p @top_line.length
-	p @middle_line.length
-	p @bottom_line.length
-else
-	final = []
-	@top_line = @top_line.zip(@middle_line, @bottom_line)
-	@top_line.flatten
+  def one_sixty_swiftly
+    if @top_line[0].length < 160
+	    p @top_line.length
+	    p @middle_line.length
+	    p @bottom_line.length
+    else
+    	final = []
+    	@top_line = @top_line.zip(@middle_line, @bottom_line)
+    	@top_line.flatten
+    end
+  end
 end
+
+split_string_test = BrailleParser.new([".....0", "0.00..", "0..0..", "0.000.", "0..0..", "......", ".00...", ".00.0.", "......", "0.....", "......", ".00.0.", ".0000.", "0.000.", ".00...", "00.00.", "0000.."])
+p split_string_test.split_string
